6855c3fbae68a503d452cdb469fad290e788aa7a
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-05-20 11:18:44 -0400

    Remove auto-gen-ed energy from UHF_CCSD

--- src/chem/ccsd/uhf_ccsd.py
+++ src/chem/ccsd/uhf_ccsd.py
@@ -1,7 +1,9 @@
 import numpy as np
-from numpy import einsum
 from numpy.typing import NDArray
 from chem.hf.intermediates_builders import Intermediates
+from chem.ccsd.equations.uhf_ccsd_energy import (
+    get_energy,
+)
 from chem.ccsd.equations.singles_uhf import (
     get_singles_residual_aa,
     get_singles_residual_bb,
@@ -240,66 +242,16 @@ class UHF_CCSD:
         return new_t_amps
 
     def get_energy(self) -> float:
-        f_aa = self.f_aa
-        f_bb = self.f_bb
-        t1_aa = self.t1_aa
-        t1_bb = self.t1_bb
-        t2_aaaa = self.t2_aaaa
-        t2_abab = self.t2_abab
-        t2_bbbb = self.t2_bbbb
-        g_aaaa = self.g_aaaa
-        g_abab = self.g_abab
-        g_bbbb = self.g_bbbb
-        oa = self.oa
-        ob = self.ob
-        va = self.va
-        vb = self.vb
-
-        uhf_ccsd = 0.0
-        uhf_ccsd += 1.00 * einsum('ii', f_aa[oa, oa])
-        uhf_ccsd += 1.00 * einsum('ii', f_bb[ob, ob])
-        uhf_ccsd += 1.00 * einsum('ia,ai', f_aa[oa, va], t1_aa)
-        uhf_ccsd += 1.00 * einsum('ia,ai', f_bb[ob, vb], t1_bb)
-        uhf_ccsd += -0.50 * einsum('jiji', g_aaaa[oa, oa, oa, oa])
-        uhf_ccsd += -0.50 * einsum('jiji', g_abab[oa, ob, oa, ob])
-        uhf_ccsd += -0.50 * einsum('ijij', g_abab[oa, ob, oa, ob])
-        uhf_ccsd += -0.50 * einsum('jiji', g_bbbb[ob, ob, ob, ob])
-        uhf_ccsd += 0.250 * einsum(
-            'jiab,abji', g_aaaa[oa, oa, va, va], t2_aaaa
-        )
-        uhf_ccsd += 0.250 * einsum(
-            'jiab,abji', g_abab[oa, ob, va, vb], t2_abab
-        )
-        uhf_ccsd += 0.250 * einsum(
-            'ijab,abij', g_abab[oa, ob, va, vb], t2_abab
-        )
-        uhf_ccsd += 0.250 * einsum(
-            'jiba,baji', g_abab[oa, ob, va, vb], t2_abab
-        )
-        uhf_ccsd += 0.250 * einsum(
-            'ijba,baij', g_abab[oa, ob, va, vb], t2_abab
-        )
-        uhf_ccsd += 0.250 * einsum(
-            'jiab,abji', g_bbbb[ob, ob, vb, vb], t2_bbbb
-        )
-        uhf_ccsd += -0.50 * einsum(
-            'jiab,ai,bj', g_aaaa[oa, oa, va, va], t1_aa, t1_aa,
-            optimize=['einsum_path', (0, 1), (0, 1)]
-        )
-        uhf_ccsd += 0.50 * einsum(
-            'ijab,ai,bj', g_abab[oa, ob, va, vb], t1_aa, t1_bb,
-            optimize=['einsum_path', (0, 1), (0, 1)]
-        )
-        uhf_ccsd += 0.50 * einsum(
-            'jiba,ai,bj', g_abab[oa, ob, va, vb], t1_bb, t1_aa,
-            optimize=['einsum_path', (0, 1), (0, 1)]
-        )
-        uhf_ccsd += -0.50 * einsum(
-            'jiab,ai,bj', g_bbbb[ob, ob, vb, vb], t1_bb, t1_bb,
-            optimize=['einsum_path', (0, 1), (0, 1)]
+        uhf_ccsd_energy = get_energy(
+            intermediates=self.intermediates,
+            t1_aa = self.t1_aa,
+            t1_bb = self.t1_bb,
+            t2_aaaa = self.t2_aaaa,
+            t2_abab = self.t2_abab,
+            t2_bbbb = self.t2_bbbb,
         )
 
-        return uhf_ccsd
+        return float(uhf_ccsd_energy)
 
     def build_dampers(self, shift_1e: float = 0.0, shift_2e: float = 0.0):
         """ Helper objects that allow you to take a `matrix` and do
