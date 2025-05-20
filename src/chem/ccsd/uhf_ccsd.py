d8d72bfa783bc9a4bed2c62bed3448cdd016b4ad
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-05-20 10:38:38 -0400

    Remove the auto-gen'ed code from the UHF_CCSD

--- src/chem/ccsd/uhf_ccsd.py
+++ src/chem/ccsd/uhf_ccsd.py
@@ -2,6 +2,10 @@ import numpy as np
 from numpy import einsum
 from numpy.typing import NDArray
 from chem.hf.intermediates_builders import Intermediates
+from chem.ccsd.equations.singles_uhf import (
+    get_singles_residual_aa,
+    get_singles_residual_bb,
+)
 from chem.ccsd.equations.doubles_uhf import (
     get_doubles_residual_aaaa,
     get_doubles_residual_abab,
@@ -192,9 +196,6 @@ class UHF_CCSD:
     def calculate_residuals(self):
         residuals = dict()
 
-        residuals['aa'] = self.get_singles_residual_aa()
-        residuals['bb'] = self.get_singles_residual_bb()
-
         kwargs = dict(
             intermediates=self.intermediates,
             t1_aa=self.t1_aa,
@@ -203,6 +204,10 @@ class UHF_CCSD:
             t2_abab=self.t2_abab,
             t2_bbbb=self.t2_bbbb,
         )
+
+        residuals['aa'] = get_singles_residual_aa(**kwargs)
+        residuals['bb'] = get_singles_residual_bb(**kwargs)
+
         residuals['aaaa'] = get_doubles_residual_aaaa(**kwargs)
         residuals['abab'] = get_doubles_residual_abab(**kwargs)
         residuals['abba'] = get_doubles_residual_abba(**kwargs)
@@ -295,236 +300,6 @@ class UHF_CCSD:
         )
 
         return uhf_ccsd
-
-    def get_singles_residual_aa(self) -> NDArray:
-        f_aa = self.f_aa
-        f_bb = self.f_bb
-        g_aaaa = self.g_aaaa
-        g_abab = self.g_abab
-        g_bbbb = self.g_bbbb
-        va = self.va
-        vb = self.vb
-        oa = self.oa
-        ob = self.ob
-        t1_aa = self.t1_aa
-        t1_bb = self.t1_bb
-        t2_aaaa = self.t2_aaaa
-        t2_abab = self.t2_abab
-
-        # The spin aa case.
-        singles_res_aa = 1.00 * einsum('ai->ai', f_aa[va, oa])
-        singles_res_aa += -1.00 * einsum('ji,aj->ai', f_aa[oa, oa], t1_aa)
-        singles_res_aa += 1.00 * einsum('ab,bi->ai', f_aa[va, va], t1_aa)
-        singles_res_aa += -1.00 * einsum('jb,baij->ai', f_aa[oa, va], t2_aaaa)
-        singles_res_aa += 1.00 * einsum('jb,abij->ai', f_bb[ob, vb], t2_abab)
-        singles_res_aa += -1.00 * einsum(
-            'jb,aj,bi->ai', f_aa[oa, va], t1_aa, t1_aa,
-            optimize=['einsum_path', (0, 1), (0, 1)]
-        )
-        singles_res_aa += 1.00 * einsum(
-            'jabi,bj->ai', g_aaaa[oa, va, va, oa], t1_aa
-        )
-        singles_res_aa += 1.00 * einsum(
-            'ajib,bj->ai', g_abab[va, ob, oa, vb], t1_bb
-        )
-        singles_res_aa += -0.50 * einsum(
-            'kjbi,bakj->ai', g_aaaa[oa, oa, va, oa], t2_aaaa
-        )
-        singles_res_aa += -0.50 * einsum(
-            'kjib,abkj->ai', g_abab[oa, ob, oa, vb], t2_abab
-        )
-        singles_res_aa += -0.50 * einsum(
-            'jkib,abjk->ai', g_abab[oa, ob, oa, vb], t2_abab
-        )
-        singles_res_aa += -0.50 * einsum(
-            'jabc,bcij->ai', g_aaaa[oa, va, va, va], t2_aaaa
-        )
-        singles_res_aa += 0.50 * einsum(
-            'ajbc,bcij->ai', g_abab[va, ob, va, vb], t2_abab
-        )
-        singles_res_aa += 0.50 * einsum(
-            'ajcb,cbij->ai', g_abab[va, ob, va, vb], t2_abab
-        )
-        singles_res_aa += 1.00 * einsum(
-            'kjbc,caik,bj->ai', g_aaaa[oa, oa, va, va], t2_aaaa, t1_aa,
-            optimize=['einsum_path', (0, 2), (0, 1)]
-        )
-        singles_res_aa += -1.00 * einsum(
-            'kjcb,caik,bj->ai', g_abab[oa, ob, va, vb], t2_aaaa, t1_bb,
-            optimize=['einsum_path', (0, 2), (0, 1)]
-        )
-        singles_res_aa += 1.00 * einsum(
-            'jkbc,acik,bj->ai', g_abab[oa, ob, va, vb], t2_abab, t1_aa,
-            optimize=['einsum_path', (0, 2), (0, 1)]
-        )
-        singles_res_aa += -1.00 * einsum(
-            'kjbc,acik,bj->ai', g_bbbb[ob, ob, vb, vb], t2_abab, t1_bb,
-            optimize=['einsum_path', (0, 2), (0, 1)]
-        )
-        singles_res_aa += 0.50 * einsum(
-            'kjbc,cakj,bi->ai', g_aaaa[oa, oa, va, va], t2_aaaa, t1_aa,
-            optimize=['einsum_path', (0, 1), (0, 1)]
-        )
-        singles_res_aa += -0.50 * einsum(
-            'kjbc,ackj,bi->ai', g_abab[oa, ob, va, vb], t2_abab, t1_aa,
-            optimize=['einsum_path', (0, 1), (0, 1)]
-        )
-        singles_res_aa += -0.50 * einsum(
-            'jkbc,acjk,bi->ai', g_abab[oa, ob, va, vb], t2_abab, t1_aa,
-            optimize=['einsum_path', (0, 1), (0, 1)]
-        )
-        singles_res_aa += 0.50 * einsum(
-            'kjbc,aj,bcik->ai', g_aaaa[oa, oa, va, va], t1_aa, t2_aaaa,
-            optimize=['einsum_path', (0, 1), (0, 1)]
-        )
-        singles_res_aa += -0.50 * einsum(
-            'jkbc,aj,bcik->ai', g_abab[oa, ob, va, vb], t1_aa, t2_abab,
-            optimize=['einsum_path', (0, 1), (0, 1)]
-        )
-        singles_res_aa += -0.50 * einsum(
-            'jkcb,aj,cbik->ai', g_abab[oa, ob, va, vb], t1_aa, t2_abab,
-            optimize=['einsum_path', (0, 1), (0, 1)]
-        )
-        singles_res_aa += 1.00 * einsum(
-            'kjbi,ak,bj->ai', g_aaaa[oa, oa, va, oa], t1_aa, t1_aa,
-            optimize=['einsum_path', (0, 2), (0, 1)]
-        )
-        singles_res_aa += -1.00 * einsum(
-            'kjib,ak,bj->ai', g_abab[oa, ob, oa, vb], t1_aa, t1_bb,
-            optimize=['einsum_path', (0, 2), (0, 1)]
-        )
-        singles_res_aa += 1.00 * einsum(
-            'jabc,bj,ci->ai', g_aaaa[oa, va, va, va], t1_aa, t1_aa,
-            optimize=['einsum_path', (0, 1), (0, 1)]
-        )
-        singles_res_aa += 1.00 * einsum(
-            'ajcb,bj,ci->ai', g_abab[va, ob, va, vb], t1_bb, t1_aa,
-            optimize=['einsum_path', (0, 1), (0, 1)]
-        )
-        singles_res_aa += 1.00 * einsum(
-            'kjbc,ak,bj,ci->ai', g_aaaa[oa, oa, va, va], t1_aa, t1_aa, t1_aa,
-            optimize=['einsum_path', (0, 2), (0, 2), (0, 1)]
-        )
-        singles_res_aa += -1.00 * einsum(
-            'kjcb,ak,bj,ci->ai', g_abab[oa, ob, va, vb], t1_aa, t1_bb, t1_aa,
-            optimize=['einsum_path', (0, 2), (0, 2), (0, 1)]
-        )
-
-        return singles_res_aa
-
-    def get_singles_residual_bb(self) -> NDArray:
-        f_aa = self.f_aa
-        f_bb = self.f_bb
-        g_aaaa = self.g_aaaa
-        g_abab = self.g_abab
-        g_bbbb = self.g_bbbb
-        va = self.va
-        vb = self.vb
-        oa = self.oa
-        ob = self.ob
-        t1_aa = self.t1_aa
-        t1_bb = self.t1_bb
-        t2_aaaa = self.t2_aaaa
-        t2_abab = self.t2_abab
-        t2_bbbb = self.t2_bbbb
-        singles_res_bb = 1.00 * einsum('ai->ai', f_bb[vb, ob])
-        singles_res_bb += -1.00 * einsum('ji,aj->ai', f_bb[ob, ob], t1_bb)
-        singles_res_bb += 1.00 * einsum('ab,bi->ai', f_bb[vb, vb], t1_bb)
-        singles_res_bb += 1.00 * einsum('jb,baji->ai', f_aa[oa, va], t2_abab)
-        singles_res_bb += -1.00 * einsum('jb,baij->ai', f_bb[ob, vb], t2_bbbb)
-        singles_res_bb += -1.00 * einsum(
-            'jb,aj,bi->ai', f_bb[ob, vb], t1_bb, t1_bb,
-            optimize=['einsum_path', (0, 2), (0, 1)]
-        )
-        singles_res_bb += 1.00 * einsum(
-            'jabi,bj->ai', g_abab[oa, vb, va, ob], t1_aa
-        )
-        singles_res_bb += 1.00 * einsum(
-            'jabi,bj->ai', g_bbbb[ob, vb, vb, ob], t1_bb
-        )
-        singles_res_bb += -0.50 * einsum(
-            'kjbi,bakj->ai', g_abab[oa, ob, va, ob], t2_abab
-        )
-        singles_res_bb += -0.50 * einsum(
-            'jkbi,bajk->ai', g_abab[oa, ob, va, ob], t2_abab
-        )
-        singles_res_bb += -0.50 * einsum(
-            'kjbi,bakj->ai', g_bbbb[ob, ob, vb, ob], t2_bbbb
-        )
-        singles_res_bb += 0.50 * einsum(
-            'jabc,bcji->ai', g_abab[oa, vb, va, vb], t2_abab
-        )
-        singles_res_bb += 0.50 * einsum(
-            'jacb,cbji->ai', g_abab[oa, vb, va, vb], t2_abab
-        )
-        singles_res_bb += -0.50 * einsum(
-            'jabc,bcij->ai', g_bbbb[ob, vb, vb, vb], t2_bbbb
-        )
-        singles_res_bb += -1.00 * einsum(
-            'kjbc,caki,bj->ai', g_aaaa[oa, oa, va, va], t2_abab, t1_aa,
-            optimize=['einsum_path', (0, 2), (0, 1)]
-        )
-        singles_res_bb += 1.00 * einsum(
-            'kjcb,caki,bj->ai', g_abab[oa, ob, va, vb], t2_abab, t1_bb,
-            optimize=['einsum_path', (0, 2), (0, 1)]
-        )
-        singles_res_bb += -1.00 * einsum(
-            'jkbc,caik,bj->ai', g_abab[oa, ob, va, vb], t2_bbbb, t1_aa,
-            optimize=['einsum_path', (0, 2), (0, 1)]
-        )
-        singles_res_bb += 1.00 * einsum(
-            'kjbc,caik,bj->ai', g_bbbb[ob, ob, vb, vb], t2_bbbb, t1_bb,
-            optimize=['einsum_path', (0, 2), (0, 1)]
-        )
-        singles_res_bb += -0.50 * einsum(
-            'kjcb,cakj,bi->ai', g_abab[oa, ob, va, vb], t2_abab, t1_bb,
-            optimize=['einsum_path', (0, 2), (0, 1)]
-        )
-        singles_res_bb += -0.50 * einsum(
-            'jkcb,cajk,bi->ai', g_abab[oa, ob, va, vb], t2_abab, t1_bb,
-            optimize=['einsum_path', (0, 2), (0, 1)]
-        )
-        singles_res_bb += 0.50 * einsum(
-            'kjbc,cakj,bi->ai', g_bbbb[ob, ob, vb, vb], t2_bbbb, t1_bb,
-            optimize=['einsum_path', (0, 2), (0, 1)]
-        )
-        singles_res_bb += -0.50 * einsum(
-            'kjbc,aj,bcki->ai', g_abab[oa, ob, va, vb], t1_bb, t2_abab,
-            optimize=['einsum_path', (0, 2), (0, 1)]
-        )
-        singles_res_bb += -0.50 * einsum(
-            'kjcb,aj,cbki->ai', g_abab[oa, ob, va, vb], t1_bb, t2_abab,
-            optimize=['einsum_path', (0, 2), (0, 1)]
-        )
-        singles_res_bb += 0.50 * einsum(
-            'kjbc,aj,bcik->ai', g_bbbb[ob, ob, vb, vb], t1_bb, t2_bbbb,
-            optimize=['einsum_path', (0, 2), (0, 1)]
-        )
-        singles_res_bb += -1.00 * einsum(
-            'jkbi,ak,bj->ai', g_abab[oa, ob, va, ob], t1_bb, t1_aa,
-            optimize=['einsum_path', (0, 2), (0, 1)]
-        )
-        singles_res_bb += 1.00 * einsum(
-            'kjbi,ak,bj->ai', g_bbbb[ob, ob, vb, ob], t1_bb, t1_bb,
-            optimize=['einsum_path', (0, 2), (0, 1)]
-        )
-        singles_res_bb += 1.00 * einsum(
-            'jabc,bj,ci->ai', g_abab[oa, vb, va, vb], t1_aa, t1_bb,
-            optimize=['einsum_path', (0, 1), (0, 1)]
-        )
-        singles_res_bb += 1.00 * einsum(
-            'jabc,bj,ci->ai', g_bbbb[ob, vb, vb, vb], t1_bb, t1_bb,
-            optimize=['einsum_path', (0, 1), (0, 1)]
-        )
-        singles_res_bb += -1.00 * einsum(
-            'jkbc,ak,bj,ci->ai', g_abab[oa, ob, va, vb], t1_bb, t1_aa, t1_bb,
-            optimize=['einsum_path', (0, 2), (1, 2), (0, 1)]
-        )
-        singles_res_bb += 1.00 * einsum(
-            'kjbc,ak,bj,ci->ai', g_bbbb[ob, ob, vb, vb], t1_bb, t1_bb, t1_bb,
-            optimize=['einsum_path', (0, 2), (1, 2), (0, 1)]
-        )
-        return singles_res_bb
 
     def build_dampers(self, shift_1e: float = 0.0, shift_2e: float = 0.0):
         """ Helper objects that allow you to take a `matrix` and do
