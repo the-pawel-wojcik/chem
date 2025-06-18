b944d88cd45a99c6848bbfcbc9c33b6cd91982e5
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-06-18 11:46:59 -0400

    feat: hf -> scf that takes geo and basis args

--- src/chem/hf/electronic_structure.py
+++ src/chem/hf/electronic_structure.py
@@ -3,16 +3,30 @@ from psi4.core import Molecule, Wavefunction
 
 
 def scf() -> tuple[Molecule, float, Wavefunction]:
-
-    mol: Molecule = psi4.geometry("""
+    """ For water at the cc-pVDZ geometry and in that basis set. """
+    return hf(geometry="""
     0 1
     O1	0.00000   0.00000   0.11572
     H2	0.00000   0.74879  -0.46288
     H3	0.00000  -0.74879  -0.46288
     symmetry c1
-    """)
+    """, basis='cc-pvdz',)
 
-    psi4.set_options({'basis': 'cc-pvdz',
+
+def hf(geometry: str, basis: str) -> tuple[Molecule, float, Wavefunction]:
+    """ The geometry string got to look like this
+    ```
+    0 1
+    O1	0.00000   0.00000   0.11572
+    H2	0.00000   0.74879  -0.46288
+    H3	0.00000  -0.74879  -0.46288
+    symmetry c1
+    ```
+    """
+
+    mol: Molecule = psi4.geometry(geometry)
+
+    psi4.set_options({'basis': basis,
                       'scf_type': 'pk',
                       'e_convergence': 1e-12,
                       'd_convergence': 1e-12})
