1e87054587fc221977822dd27defb2e787dfb1ee
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-06-23 17:27:23 -0400

    feat: start the Spin_MBE class

--- src/chem/ccsd/containers.py
+++ src/chem/ccsd/containers.py
@@ -1,5 +1,8 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from enum import StrEnum, auto
+import numpy as np
 from numpy.typing import NDArray
+
 
 @dataclass
 class UHF_CCSD_Lambda_Data:
@@ -21,3 +24,37 @@ class UHF_CCSD_Data:
     t2_baba: NDArray
 
     lmbda: UHF_CCSD_Lambda_Data | None = None
+
+
+class E1_spin(StrEnum):
+    aa = auto()
+    bb = auto()
+
+
+class E2_spin(StrEnum):
+    aaaa = auto()
+    abab = auto()
+    abba = auto()
+    baab = auto()
+    baba = auto()
+    bbbb = auto()
+
+
+@dataclass
+class Spin_MBE():
+    """ MBE stands for many body expansion. """
+    singles: dict[E1_spin, NDArray] = field(default_factory=dict)
+    doubles: dict[E2_spin, NDArray] = field(default_factory=dict)
+
+    def pretty_print_mbe(self) -> None:
+        with np.printoptions(precision=3, suppress=True):
+
+            print("Singles:")
+            for key, value in self.singles.items():
+                print(f' {key} shape = {value.shape}')
+                print(value)
+
+            print("Doubles:")
+            for key, value in self.doubles.items():
+                print(f' {key} shape = {value.shape}')
+            print("")
