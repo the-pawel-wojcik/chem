701b3604a2980a243e5b40a1de15dadd118e3b35
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-07-01 16:13:36 -0400

    test: move test_ccsd to tests + update readme

--- readme.md
+++ readme.md
@@ -1,2 +1,5 @@
 # Computational Chemistry
 Electronic structure programs
+
+This package depends on [psi4](https://psicode.org/), this dependency is not
+listed in the `pyproject.toml` as `psi4` is unavailable from pypi.
---
+++ tests/ccsd/test_ccsd.py
@@ -0,0 +1,43 @@
+from chem.ccsd.uhf_ccsd import UHF_CCSD
+from chem.hf.electronic_structure import hf, ResultHF
+from chem.hf.intermediates_builders import Intermediates, extract_intermediates
+import pytest
+import numpy as np
+
+
+@pytest.fixture(scope='session')
+def hf_result() -> ResultHF:
+    """ Geometry from CCCBDB: HF/STO-3G """
+    geometry = """
+    0 1
+    O1	0.0000   0.0000   0.1272
+    H2	0.0000   0.7581  -0.5086
+    H3	0.0000  -0.7581  -0.5086
+    symmetry c1
+    """
+    hf_result = hf(geometry=geometry, basis='sto-3g')
+    return hf_result
+
+
+@pytest.fixture(scope='session')
+def nuclear_repulsion_energy(hf_result: ResultHF) -> float:
+    nuclear_repulsion_energy = hf_result.molecule.nuclear_repulsion_energy()
+    return nuclear_repulsion_energy
+
+
+@pytest.fixture(scope='session')
+def intermediates(hf_result) -> Intermediates:
+    return extract_intermediates(hf_result.wfn)
+
+
+def test_ccsd_energy(
+    intermediates: Intermediates,
+    nuclear_repulsion_energy: float
+):
+    ccsd = UHF_CCSD(intermediates, use_diis=False)
+    ccsd.verbose = 1
+    ccsd.solve_cc_equations()
+    uhf_ccsd_energy = ccsd.get_energy()
+    uhf_ccsd_total_energy = uhf_ccsd_energy + nuclear_repulsion_energy
+    assert np.isclose(uhf_ccsd_energy, -83.9266502349831, atol=1e-5)
+    assert np.isclose(uhf_ccsd_total_energy, -75.02028564818042, atol=1e-5)
