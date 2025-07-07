3970527b212bc804ff973cb79b3f14bb4c7fdb13
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-07-07 11:57:42 -0400

    feat: GHF

---
+++ src/chem/hf/ghf_data.py
@@ -0,0 +1,147 @@
+from dataclasses import dataclass
+import numpy as np
+from numpy.typing import NDArray
+from psi4.core import Wavefunction, Matrix, MintsHelper
+
+
+@dataclass
+class GHF_Data:
+    mu_x: NDArray
+    mu_y: NDArray
+    mu_z: NDArray
+    identity_singles: NDArray
+    nmo: int
+    no: int
+    nv: int
+    v: slice
+    o: slice
+    f: NDArray
+    g: NDArray
+
+
+def _tei_to_spinorbitals(tei: NDArray, order: NDArray) -> NDArray:
+    """ Take the electron repulsion integrals `tei` aka two-electron integrals
+    in the chemists notation:
+        (s,p|q,r) = ∫∫ φs(r1) φp(r1) (1/r12) φq(r2) φr(r2) dr1 dr2                                        
+    and double the size of this matrix by taking it from the basis of orbitals
+    to spinorbitals. 
+
+    Assumes that the orbitals come from RHF calculations so the up and down
+    orbitals are exactly the same.
+
+    The `tei` is expressed in RHF spatial MOs. The `order` matrix reorderes the
+    spin-orbitals into the energy order after the spin-orbitals were created by
+    stacking the up MOs first and down MOs second.
+    """
+    assert len(tei.shape) == 4
+    nmo = 2 * tei.shape[0]
+    for dim in tei.shape:
+        assert nmo//2 == dim
+    assert len(order) == nmo
+
+    tei_ghf = np.zeros(shape=(nmo, nmo, nmo, nmo))
+    up = slice(0, nmo//2, None)
+    down = slice(nmo//2, nmo, None)
+
+    # there are 16 spin-combinations, yet only two of them are non-zero
+    tei_ghf[up, up, up, up] = tei
+    # tei_ghf[up, up, up, down] = 0
+    # tei_ghf[up, up, down, up] = 0
+    tei_ghf[up, up, down, down] = tei
+    # tei_ghf[up, down, up, up] = 0
+    # tei_ghf[up, down, up, down] = 0
+    # tei_ghf[up, down, down, up] = 0
+    # tei_ghf[up, down, down, down] = 0
+    # tei_ghf[down, up, up, up] = 0
+    # tei_ghf[down, up, up, down] = 0
+    # tei_ghf[down, up, down, up] = 0
+    # tei_ghf[down, up, down, down] = 0
+    tei_ghf[down, down, up, up] = tei
+    # tei_ghf[down, down, up, down] = 0
+    # tei_ghf[down, down, down, up] = 0
+    tei_ghf[down, down, down, down] = tei
+
+    # reorder from (all up, all down) order to the (increasing energy) order
+    # try in 1D, then extend to 2D and you will see
+    tei_ghf = np.asarray(
+        [
+            [
+                [ 
+                    row[order] for row in wall[order] 
+                ] for wall in cube[order]
+            ] for cube in tei_ghf[order]
+        ]
+    )
+    return tei_ghf
+
+
+def wfn_to_GHF_Data(wfn: Wavefunction) -> GHF_Data:
+    """ wfn is the result of RHF calculations. Here the spin up and spin down
+    orbitals will be combined to form the spin-orbitals for GHF. """
+    nmo = 2 * wfn.nmo()  # number of molecular orbitals
+    no = 2 * wfn.nalpha()  # number of occupied molecular orbitals
+    nv = nmo - no  # number of virtual molecular orbitals
+    occ = slice(None, no)
+    vir = slice(no, None)
+
+    # orbital energies
+    orbital_energies = np.concatenate((
+        wfn.epsilon_a().to_array(),
+        wfn.epsilon_b().to_array(),
+    ))
+    order = np.argsort(orbital_energies)
+    # orbitals = np.hstack((mos_up, mos_up))[:, order]  # not needed
+    fock = np.diag(orbital_energies[order])
+
+    # molecular orbitals up and down would be the same for RHF, so I use only
+    # one of them
+    mos_up: Matrix = wfn.Ca()
+
+    # use Psi4's MintsHelper to generate ERIs and dipole integrals
+    mints = MintsHelper(wfn.basisset())
+
+    # build the electron integrals in the chemists' notation
+    tei = np.asarray(mints.mo_eri(mos_up, mos_up, mos_up, mos_up))
+    tei_ghf = _tei_to_spinorbitals(tei, order)
+    tei_ghf = tei_ghf.transpose(0, 2, 1, 3) - tei_ghf.transpose(0, 2, 3, 1)
+    # antisymmetrized integrals in physicists' notation
+    # np.set_printoptions(precision=3, suppress=True)
+    # print(f'{orbitals.shape=}')
+    # for column in range(14):
+    #     print(f'{orbitals[:, column]}')
+
+    identity_singles = np.eye(nmo)
+
+    # dipole integrals
+    mu = mints.ao_dipole()
+
+    mu_x = np.asarray(mu[0])
+    mu_y = np.asarray(mu[1])
+    mu_z = np.asarray(mu[2])
+
+    # transform the dipole integrals to the MO basis
+    mos_up = np.asarray(mos_up)
+
+    mua_x = mos_up.T @ mu_x @ mos_up
+    mua_y = mos_up.T @ mu_y @ mos_up
+    mua_z = mos_up.T @ mu_z @ mos_up
+
+    mu_x = np.block([
+        [mua_x, mua_x * 0.0],
+        [mua_x * 0.0, mua_x],
+    ])
+
+
+    return GHF_Data(
+        mu_x=mua_x,
+        mu_y=mua_y,
+        mu_z=mua_z,
+        identity_singles=identity_singles,
+        f=fock,
+        o=occ,
+        v=vir,
+        nmo=nmo,
+        no=no,
+        nv=nv,
+        g=tei_ghf,
+    )
---
+++ tests/hf/test_ghf.py
@@ -0,0 +1,48 @@
+import pytest
+from chem.hf.ghf_data import wfn_to_GHF_Data, GHF_Data
+from chem.hf.electronic_structure import hf, ResultHF
+from numpy import einsum
+import numpy as np
+
+
+@pytest.fixture(scope='session')
+def hf_result() -> ResultHF:
+    """ Geometry from CCCBDB: HF/STO-3G """
+    geometry = """
+    0 1
+    O1	0.0000   0.0000   0.1272
+    H2	0.0000   0.7581  -0.5086
+    H3	0.0000  -0.7581  -0.5086
+    symmetry c1
+    """
+    hf_result = hf(geometry=geometry, basis='sto-3g')
+    return hf_result
+
+
+def get_hf_energy(ghf_data: GHF_Data) -> float:
+    f = ghf_data.f
+    g = ghf_data.g
+    o = ghf_data.o
+    energy = 1.00 * einsum('ii', f[o, o])
+    energy += -0.50 * einsum('jiji', g[o, o, o, o])
+    return float(energy)
+
+
+def test_constructor(hf_result: ResultHF):
+    wfn_to_GHF_Data(hf_result.wfn)
+
+
+def test_energies(hf_result: ResultHF):
+    ghf_data = wfn_to_GHF_Data(hf_result.wfn)
+    assert ghf_data.f.shape == (14, 14)
+    fock_diagonal = np.array([
+        -20.25157669, -20.25157669,  -1.25754098, -1.25754098,  -0.59385141,
+        -0.59385141, -0.45972452,  -0.45972452, -0.3926153 ,  -0.3926153,
+        0.58177897, 0.58177897, 0.69266369,   0.69266369
+    ])
+    assert np.allclose(fock_diagonal, ghf_data.f.diagonal(), atol=1e-7)
+    energy = get_hf_energy(ghf_data)
+    assert np.isclose(-83.87226577852897, energy, atol=1e-7)
+    nre = hf_result.molecule.nuclear_repulsion_energy()
+    # assert np.isclose(nre, 8.906479, atol=1e-5)  # value from CCCBDB
+    assert np.isclose(energy + nre, -74.965901, atol=1e-5)  # value from CCCBDB
