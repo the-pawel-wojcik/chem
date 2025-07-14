a2d9492496e9713ed64536982963bef98bcddd4b
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-07-14 09:47:49 -0400

    test: CCSD energy vs Psi4

--- tests/ccsd/test_ghf_ccsd_water_sto3g.py
+++ tests/ccsd/test_ghf_ccsd_water_sto3g.py
@@ -5,14 +5,22 @@ import pytest
 import numpy as np
 
 
+PSI4_NRE_H2O_STO3G = 8.9064754830195323
+PSI4_CCSD_ENERGY_H2O_STO3G = -75.020284329413670
+
+
 @pytest.fixture(scope='session')
 def nuclear_repulsion_energy(water_sto3g: ResultHF) -> float:
     nuclear_repulsion_energy = water_sto3g.molecule.nuclear_repulsion_energy()
     return nuclear_repulsion_energy
 
 
+def test_nuclear_repulsion_energy(nuclear_repulsion_energy: float) -> None:
+    assert np.isclose(nuclear_repulsion_energy, PSI4_NRE_H2O_STO3G, 1e-10)
+
+
 @pytest.fixture(scope='session')
-def ghf_data(water_sto3g: ResultHF) -> GHF_Data:
+def ghf_data_water_sto3g(water_sto3g: ResultHF) -> GHF_Data:
     return wfn_to_GHF_Data(water_sto3g.wfn)
 
 
@@ -24,16 +32,24 @@ def ghf_data(water_sto3g: ResultHF) -> GHF_Data:
     ]
 )
 def test_ccsd_energy(
-    ghf_data: GHF_Data,
+    ghf_data_water_sto3g: GHF_Data,
     nuclear_repulsion_energy: float,
     ghf_ccsd_config: GHF_CCSD_Config,
 ) -> None:
-    ccsd = GHF_CCSD(ghf_data, ghf_ccsd_config)
+    ccsd = GHF_CCSD(ghf_data_water_sto3g, ghf_ccsd_config)
     ccsd.solve_cc_equations()
     ghf_ccsd_energy = ccsd.get_energy()
+    assert np.isclose(
+        ghf_ccsd_energy,
+        PSI4_CCSD_ENERGY_H2O_STO3G - PSI4_NRE_H2O_STO3G,
+        atol=1e-10
+    )
     ghf_ccsd_total_energy = ghf_ccsd_energy + nuclear_repulsion_energy
-    assert np.isclose(ghf_ccsd_energy, -83.9266502349831, atol=1e-5)
-    assert np.isclose(ghf_ccsd_total_energy, -75.02028564818042, atol=1e-5)
+    assert np.isclose(
+        ghf_ccsd_total_energy,
+        PSI4_CCSD_ENERGY_H2O_STO3G,
+        atol=1e-10
+    )
 
 
 # def test_ccsd_diis_energy(
@@ -48,6 +64,6 @@ def test_ccsd_energy(
 #     assert np.isclose(ghf_ccsd_total_energy, -75.02028564818042, atol=1e-5)
 
 
-def test_lambda_solver(ghf_data: GHF_Data) -> None:
-    ccsd = GHF_CCSD(ghf_data, config=GHF_CCSD_Config(verbose=True))
+def test_lambda_solver(ghf_data_water_sto3g: GHF_Data) -> None:
+    ccsd = GHF_CCSD(ghf_data_water_sto3g, config=GHF_CCSD_Config(verbose=True))
     ccsd.solve_lambda_equations()
