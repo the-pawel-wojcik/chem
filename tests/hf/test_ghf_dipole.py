c5cd8a61cf4fbcdae72681646b58d817ab0448ab
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-07-25 16:00:07 -0400

    test: GHF elements of the dipole moment

--- tests/hf/test_ghf_dipole.py
+++ tests/hf/test_ghf_dipole.py
@@ -6,6 +6,56 @@ from chem.meta.coordinates import CARTESIAN, Descartes
 import numpy as np
 
 
+def test_dipole_elements_HF_water_sto3g(water_sto3g: ResultHF) -> None:
+    ghf_data: GHF_Data = wfn_to_GHF_Data(water_sto3g.wfn)
+    o = ghf_data.o
+    v = ghf_data.v
+
+    PSI4_DIPOLE = {
+        Descartes.x: np.array([
+            [0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0],
+            [0.070510953, 0.0, 0.0, 0.0],
+            [0.0, 0.070510953, 0.0, 0.0],
+        ]),
+        Descartes.y: np.array([
+            [0.0, 0.0, -0.055109142700219, 0.0],
+            [0.0, 0.0, .0, -0.055109142700219],
+            [0.0, 0.0, -0.122193314873649, 0.0],
+            [0.0, 0.0, 0.0, -0.122193314873649],
+            [-0.810982420713625, 0.0, 0.0, 0.0],
+            [0.0, -0.810982420713625, 0.0, 0.0],
+            [0.0, 0.0, 0.693483744632154, 0.0],
+            [0.0, 0.0, 0.0, 0.693483744632154],
+            [0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0],
+        ]),
+        Descartes.z: np.array([
+            [-0.045086293393440, 0.0, 0.0, 0.0],
+            [0.0, -0.045086293393440, 0.0, 0.0],
+            [-0.111816110709066, 0.0, 0.0, 0.0],
+            [0.0, -0.111816110709066, 0.0, 0.0],
+            [0.0, 0.0, -0.679589465357873, 0.0],
+            [0.0, 0.0, 0.0, -0.679589465357873],
+            [0.545551238783468, 0.0, 0.0, 0.0],
+            [0.0, 0.545551238783468, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0],
+        ]),
+    }
+
+    for direction in Descartes:
+        assert np.allclose(
+            ghf_data.mu[direction][o, v], PSI4_DIPOLE[direction], atol=1e-8
+        )
+
+
 def test_dipole_HF_water_sto3g(water_sto3g: ResultHF) -> None:
     mol = water_sto3g.molecule
     ghf_data: GHF_Data = wfn_to_GHF_Data(water_sto3g.wfn)
